4bc6898059c5b897628f00b6eaf23e71ec551d49
MantasSil <mantas.silanskas.ms@gmail.com>
2022-10-22 18:05:34 +0300

    Fixed go mod file

--- cmd/main.go
+++ cmd/main.go
@@ -4,7 +4,8 @@ import (
 	"fmt"
 	"os"
 	"path/filepath"
-	"tomitaXmlParser/internal"
+
+	"github.com/MantasSilanskas/Tomita-EMOS-XML-Parcer/internal"
 )
 
 func main() {
--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
-module tomitaXmlParser
+module github.com/MantasSilanskas/Tomita-EMOS-XML-Parcer
 
 go 1.18
